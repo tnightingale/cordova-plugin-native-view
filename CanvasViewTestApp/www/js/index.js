59b2f2ea4355ef5f6aeaba5b38ee90c4febcf937
Tom Nightingale <tom@tnightingale.com>
2016-05-13 11:32:13 -0700

    Added test cordova application.

---
+++ CanvasViewTestApp/www/js/index.js
@@ -0,0 +1,8 @@
+document.addEventListener('deviceready', init);
+
+function init() {
+    console.log('Loaded!');
+    // document.body.addEventListener("click", function () {
+    //     new CanvasView.View(document.getElementById("map"), 'test', true);
+    // });
+}
